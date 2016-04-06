0bf09b56bd4bc8728071c7a949927e5c72ca0902
aliccce <aliceg@outlook.com>
2016-04-06 17:09:03 +0200

    removed activate_temperature, integrated in temperature

--- userpage/urls.py
+++ userpage/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
 
     url(r'^status/$', views.status, name="status"),
     url(r'^temperature/$', views.temperature, name="temperature"),
-    url(r'^activatetemperature/$', views.activate_temperature)
 
 ]
 
